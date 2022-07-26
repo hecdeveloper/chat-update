6b58fd50923b6df42fb9bbe4dc8f1e0bbc734444
Hector Ibarra <hector@oneseventech.com>
2022-07-26 13:04:08 -0400

    icons

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React from "react";
-import { BeakerIcon, HomeIcon, SparklesIcon } from "@heroicons/react/solid";
+import { BeakerIcon, HomeIcon, SparklesIcon, AnnotationIcon, ChatAlt2Icon, ChartPieIcon } from "@heroicons/react/solid";
 
 const Nav = () => {
   return (
@@ -10,7 +10,7 @@ const Nav = () => {
         aria-label="Sidebar"
       >
         <ul className="flex flex-col space-y-2 rounded">
-          <Link href="/elstrim">
+          <Link href="/">
             <li className="flex items-center p-2 py-3 text-base font-normal text-white rounded-l-full hover:bg-teal-400">
               <a className="flex ml-3 ">
                 <HomeIcon className="w-5 h-5 mr-2 " /> Dashboard{" "}
@@ -18,25 +18,25 @@ const Nav = () => {
             
             </li>
           </Link>
-          <Link href="/elstrim">
+          <Link href="/">
             <li className="flex items-center p-2 py-3 text-base font-normal text-white rounded-l-full hover:bg-teal-400">
               <a className="flex ml-3 ">
-                <HomeIcon className="w-5 h-5 mr-2 " /> Dashboard{" "}
+                <ChartPieIcon className="w-5 h-5 mr-2 " /> Analytics{" "}
               </a>
             
             </li>
           </Link>
-          <Link href="/elstrim">
+          <Link href="/">
             <li className="flex items-center p-2 py-3 text-base font-normal text-white rounded-l-full hover:bg-teal-400">
               <a className="flex ml-3 ">
-                <HomeIcon className="w-5 h-5 mr-2 " /> Dashboard{" "}
+                <AnnotationIcon className="w-5 h-5 mr-2 " /> SMS Actions{" "}
               </a>
             
             </li>
           </Link>
           <li className="py-3 rounded-l-full text-slate-500 bg-slate-100">
             <a className="flex ml-3">
-              <SparklesIcon className="w-5 h-5 text-slate-500" /> Online Reviews{" "}
+              <ChatAlt2Icon className="w-5 h-5 text-slate-500" /> Chat{" "}
             </a>
           </li>
         </ul>
